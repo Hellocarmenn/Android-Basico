1f46d7dd71bf25e2f8c62a585c26990a8871b872
Carmen <carmenmtz97@comunidad.unam.mx>
2022-06-16 12:26:26 -0500

    Inicio android

--- Colecciones/app/src/main/java/com/example/colecciones/MainActivity.kt
+++ Colecciones/app/src/main/java/com/example/colecciones/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.colecciones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    //Agregando codigo
+    val TAG ="Miactividad"
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        var name = "Carmen"
+        Log.d(TAG,"Log de depuracion")
+        Log.e(TAG,"Log de Error")
+        Log.v(TAG,"Log de registro")
+        Log.w(TAG,"Log de Advertencia")
+        Log.i(TAG,"Log de informaciòn")
+        name = "Ricardo"
     }
+
 }
 //hola
